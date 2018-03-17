b845940b783f4e3ddde29a6be63827099a80ec70
Suraj Kumar Mall <surajmall874@gmail.com>
2018-03-17 10:49:34 +0530

    updating home.php in pages folder

--- Website/pages/home.php
+++ Website/pages/home.php
@@ -55,7 +55,8 @@
 		$user_id = $row['user_id'];
 		$user_name = $row['user_name'];
 		$user_dob = $row['user_dob'];
-		$user_gender = $row['user_gender'];
+		$u_email = $row['user_email'];
+		
 	?>
     <div id="wrapper">
 
@@ -158,6 +159,13 @@
                 <!-- /.col-lg-12 -->
             </div>
             <!-- /.row -->
+			<?php
+			
+				global $con;
+				$get_posts = "SELECT * FROM posts where parent_id ='$user_id'";
+				$run_posts = mysqli_query($con,$get_posts);
+				$cnt = mysqli_num_rows($run_posts);
+			?>
             <div class="row">
                 <div class="col-lg-3 col-md-6">
                     <div class="panel panel-primary">
@@ -167,12 +175,12 @@
                                     <i class="fa fa-comments fa-5x"></i>
                                 </div>
                                 <div class="col-xs-9 text-right">
-                                    <div class="huge">26</div>
-                                    <div>New Comments!</div>
+                                    <div class="huge"><?php echo $cnt; ?></div>
+                                    <div>Total Suspecious Posts!</div>
                                 </div>
                             </div>
                         </div>
-                        <a href="#">
+                        <a href="#lejao">
                             <div class="panel-footer">
                                 <span class="pull-left">View Details</span>
                                 <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
@@ -268,92 +276,63 @@
                             <i class="fa fa-clock-o fa-fw"></i> Posts may contain Negativity 
                         </div>
                         <!-- /.panel-heading -->
-                        <div class="panel-body">
+                        <div class="panel-body" id="lejao">
                             <ul class="timeline">
-                                <li>
-                                    <div class="timeline-badge"><i class="fa fa-check"></i>
-                                    </div>
-                                    <div class="timeline-panel">
-                                        <div class="timeline-heading">
-                                            <h4 class="timeline-title">Lorem ipsum dolor</h4>
-                                            <p><small class="text-muted"><i class="fa fa-clock-o"></i> 11 hours ago via Twitter</small>
+                            <?php
+								
+							global $con;
+							$get_posts = "SELECT * FROM posts where parent_id ='$user_id'";
+							$run_posts = mysqli_query($con,$get_posts);
+							while($row_posts = mysqli_fetch_array($run_posts)){
+							$post_id = $row_posts['post_id'];  
+							$user_id = $row_posts['user_id'];  
+							$parent_id = $row_posts['parent_id'];
+							$content = $row_posts['content'];
+							$flag = $row_posts['flag'];			
+							$post_time = $row_posts['post_time'];  
+		
+							//getting the user who has posted the thread
+							$user = "SELECT * FROM users WHERE user_id='$user_id'";
+							$run_user = mysqli_query($con,$user);
+							$row_user=mysqli_fetch_array($run_user);
+							$user_name = $row_user['user_name'];
+								//displaying post 
+								if($flag==0){
+								echo "
+									<li class='timeline-inverted'>
+										<div class='timeline-badge warning'><i class='fa fa-check'></i>
+										</div>
+										<div class='timeline-panel'>
+                                        <div class='timeline-heading'>
+                                            <h4 class='timeline-title'>$user_name</h4>
+                                            <p><small class='text-muted'><i class='fa fa-clock-o'></i> $post_time</small>
                                             </p>
                                         </div>
-                                        <div class="timeline-body">
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero laboriosam dolor perspiciatis omnis exercitationem. Beatae, officia pariatur? Est cum veniam excepturi. Maiores praesentium, porro voluptas suscipit facere rem dicta, debitis.</p>
+                                        <div class='timeline-body'>
+                                            <p>$content</p>
                                         </div>
-                                    </div>
-                                </li>
-                                <li class="timeline-inverted">
-                                    <div class="timeline-badge warning"><i class="fa fa-credit-card"></i>
-                                    </div>
-                                    <div class="timeline-panel">
-                                        <div class="timeline-heading">
-                                            <h4 class="timeline-title">Lorem ipsum dolor</h4>
+										</div>
+									</li>
+								";}
+								else
+								{
+									echo "<li>
+										<div class='timeline-badge warning'><i class='fa fa-check'></i>
+										</div>
+										<div class='timeline-panel'>
+                                        <div class='timeline-heading'>
+                                            <h4 class='timeline-title'>$user_name</h4>
+                                            <p><small class='text-muted'><i class='fa fa-clock-o'></i> $post_time</small>
+                                            </p>
                                         </div>
-                                        <div class="timeline-body">
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem dolorem quibusdam, tenetur commodi provident cumque magni voluptatem libero, quis rerum. Fugiat esse debitis optio, tempore. Animi officiis alias, officia repellendus.</p>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium maiores odit qui est tempora eos, nostrum provident explicabo dignissimos debitis vel! Adipisci eius voluptates, ad aut recusandae minus eaque facere.</p>
+                                        <div class='timeline-body'>
+                                            <p>$content</p>
                                         </div>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="timeline-badge danger"><i class="fa fa-bomb"></i>
-                                    </div>
-                                    <div class="timeline-panel">
-                                        <div class="timeline-heading">
-                                            <h4 class="timeline-title">Lorem ipsum dolor</h4>
-                                        </div>
-                                        <div class="timeline-body">
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus numquam facilis enim eaque, tenetur nam id qui vel velit similique nihil iure molestias aliquam, voluptatem totam quaerat, magni commodi quisquam.</p>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li class="timeline-inverted">
-                                    <div class="timeline-panel">
-                                        <div class="timeline-heading">
-                                            <h4 class="timeline-title">Lorem ipsum dolor</h4>
-                                        </div>
-                                        <div class="timeline-body">
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates est quaerat asperiores sapiente, eligendi, nihil. Itaque quos, alias sapiente rerum quas odit! Aperiam officiis quidem delectus libero, omnis ut debitis!</p>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="timeline-badge info"><i class="fa fa-save"></i>
-                                    </div>
-                                    <div class="timeline-panel">
-                                        <div class="timeline-heading">
-                                            <h4 class="timeline-title">Lorem ipsum dolor</h4>
-                                        </div>
-                                        <div class="timeline-body">
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis minus modi quam ipsum alias at est molestiae excepturi delectus nesciunt, quibusdam debitis amet, beatae consequuntur impedit nulla qui! Laborum, atque.</p>
-                                            <hr>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="timeline-panel">
-                                        <div class="timeline-heading">
-                                            <h4 class="timeline-title">Lorem ipsum dolor</h4>
-                                        </div>
-                                        <div class="timeline-body">
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi fuga odio quibusdam. Iure expedita, incidunt unde quis nam! Quod, quisquam. Officia quam qui adipisci quas consequuntur nostrum sequi. Consequuntur, commodi.</p>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li class="timeline-inverted">
-                                    <div class="timeline-badge success"><i class="fa fa-graduation-cap"></i>
-                                    </div>
-                                    <div class="timeline-panel">
-                                        <div class="timeline-heading">
-                                            <h4 class="timeline-title">Lorem ipsum dolor</h4>
-                                        </div>
-                                        <div class="timeline-body">
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt obcaecati, quaerat tempore officia voluptas debitis consectetur culpa amet, accusamus dolorum fugiat, animi dicta aperiam, enim incidunt quisquam maxime neque eaque.</p>
-                                        </div>
-                                    </div>
-                                </li>
+										</div>
+									</li>";
+								}
+								}
+							?>                 
                             </ul>
                         </div>
                         <!-- /.panel-body -->
